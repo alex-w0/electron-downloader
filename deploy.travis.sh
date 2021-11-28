6a150873471851f203372fb3cebbb74bb67ba4d5
Alex Wieland <wieland.alex1996@gmail.com>
2021-11-28 17:18:33 +0100

    test

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+
+echo GH_TOKEN
 # if [ "$TRAVIS_OS_NAME" == osx ]; then
 #     # deploy on mac
-#     yarn release
+     yarn publish
 # else
     # deploy on windows and linux
-    docker run --rm -e $GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn publish"
+    # docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn publish"
 # fi
